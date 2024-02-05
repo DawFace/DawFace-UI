14ca38dd2c22783df6c33bb397eef1a85291f902
andyDevMdg <andyavotiana@gmail.com>
2024-02-05 23:23:39 +0300

    style: implement login interface

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,14 +3,20 @@ import LoginForm from './LoginForm';
 
 const Login = () => {
   const [headerHeight, setHeaderHeight] = useState('h-5/6');
+  const [showLoginForm, setShowLoginForm] = useState(false)
+  const [loginFormScale, setLoginFormScale] = useState('scale-0')
+  const [loginDisplay, setLoginDisplay] = useState('hidden')
+
 
   const toggleLogin = () => {
     headerHeight === 'h-5/6' ? setHeaderHeight('h-full') : setHeaderHeight('h-5/6');
+    showLoginForm === true ? setShowLoginForm(false) : setShowLoginForm(true);
+    loginFormScale === 'scale-0' ? setLoginFormScale('scale-100') : setLoginFormScale('scale-0');
+    loginDisplay === 'hidden' ? setLoginDisplay('display') : setLoginDisplay('hidden')
   };
 
   return (
     <div
-      onClick={toggleLogin}
       className="bg-gradient-to-r 
       from-red-500 
       via-blue-400 
@@ -22,32 +28,28 @@ const Login = () => {
           transition-all
         bg-white 
           md:w-[800px] 
-          ${headerHeight} 
+          ${headerHeight}
           sm:w-full 
           drop-shadow-2xl 
           z-40
           transition-all ease-out duration-500`}
       >
-        <div className="bg-secondary h-[480px] m-3"></div>
+        <div className="bg-secondary h-[480px] w-[98%] m-2"></div>
 
         <div className="text-center flex flex-col items-center">
           <p className="md:text-5xl sm:text-4xl mt-10 select-none">Welcome to DawFace</p>
           <p className="text-xl mt-3 text-gray-400 select-none">Your face recognition system</p>
         </div>
         
-        <LoginForm />
+        <div className={`${loginFormScale} transition-all duration-500 ease-out`}><LoginForm /></div>
+
+        <div onClick={toggleLogin}
+          className='bg-gray-700 
+          w-1/6 h-[7px] rounded-full 
+          absolute bottom-3 left-0 right-0 m-auto
+          hover:cursor-pointer'></div>
       </header>
 
-      <button
-        onClick={toggleLogin}
-        className="bg-secondary 
-          h-[80px] w-[80px] 
-          absolute m-auto bottom-12 left-0 right-0 
-          rounded-full 
-          z-10
-          hover:scale-110
-          transition-scale ease-out duration-300"
-      ></button>
     </div>
   );
 };
