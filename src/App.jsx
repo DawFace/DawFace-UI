222d3c0ba0d8ba486d506be1fd46f873f3f66c47
tanjonaaa <hei.tanjona@gmail.com>
2024-02-06 11:24:04 +0100

    fix: use of react-router-dom components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Router from './routes/Router';
+import Navbar from './components/Navbar.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <Router />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
