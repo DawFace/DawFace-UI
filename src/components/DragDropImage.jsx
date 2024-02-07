1f73983e119805a715c68dfb90086cef43946387
andyDevMdg <andyavotiana@gmail.com>
2024-02-07 08:12:40 +0300

    feat: add drag and drop functions

--- src/components/DragDropImage.jsx
+++ src/components/DragDropImage.jsx
@@ -1,23 +1,67 @@
-import { useState } from "react";
+import Navbar from './Navbar';
+import { useRef, useState } from 'react';
 
 const DragAndDropImageUploader = () => {
   const [uploadedImg, setUploadedImg] = useState(null);
+  const inputRef = useRef();
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div className="flex justify-center items-center w-full h-dvh">
+      <div className="absolute top-0">
+        <Navbar />
+      </div>
+
       <form action="">
-        <div className="bg-slate-100 flex flex-col items-center w-fit p-5 drop-shadow-2xl">
-          <div className="bg-none
+        <div className="bg-secondary text-white text-center py-2 text-xl">
+          <p>Upload image for face recognition</p>
+        </div>
+        <div className="bg-tertiary flex flex-col items-center w-fit p-5 drop-shadow-2xl">
+          {!uploadedImg && (
+            <div
+              className="bg-none
             flex flex-col items-center justify-center 
             w-[360px] p-5 h-[360px]
-            border-2 border-dashed border-secondary rounded-md">
-            <p>Drag and drop here</p>
-            <p>or</p>
-          <button className="px-8 py-2 bg-secondary shadow-sm text-white tracking-wide rounded-full w-fit hover:opacity-90">Upload image</button>
-          </div>
-          <button type="submit"
-            className="mt-5 px-8 py-2 bg-secondary shadow-sm text-white tracking-wide rounded-full w-fit hover:opacity-90"
-            >Recognize</button>
+            border-2 border-dashed border-secondary rounded-md"
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+            >
+              <p>Drag and drop here</p>
+              <p>or</p>
+              <input 
+                type="file" 
+                multiple 
+                onChange={(event) => setUploadedImg(event.target.uploadedImg)}
+                hidden 
+                ref={inputRef}/>
+              <button
+                className="px-8 py-2 bg-secondary shadow-sm text-white tracking-wide 
+                rounded-full w-fit 
+                hover:opacity-90
+                transition-opacity ease-out"
+                onClick={() => inputRef.current.click()}
+              >
+                Upload image
+              </button>
+            </div>
+          )}
+          <button
+            type="submit"
+            className="mt-5 px-8 py-2 bg-secondary shadow-sm text-white tracking-wide 
+            rounded-full w-fit 
+            hover:opacity-90
+            transition-colors ease-out"
+  
+          >
+            Recognize
+          </button>
         </div>
       </form>
     </div>
