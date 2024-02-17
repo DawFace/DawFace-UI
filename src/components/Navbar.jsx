376cf059b97732fea26d37658c18c866685abfb2
andyDevMdg <andyavotiana@gmail.com>
2024-02-17 22:24:48 +0300

    style: change the navbar UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,14 @@ const Navbar = () => {
   return (
     <>
       <div className="w-screen py-2 px-5 flex items-center justify-between">
-        <Link to={'/'} className="font-extrabold tracking-tighter text-3xl">
+        <Link to={'/'} className="text-white font-extrabold tracking-tighter text-3xl">
           DawFace
         </Link>
         <div>
           <Link
             to={'/login'}
-            className="bg-tertiary font-semibold 
-            px-8 py-2 rounded-full w-fit
-            hover:bg-secondary hover:text-white
+            className="text-white py-2 px-4 w-full rounded-lg 
+            bg-primary hover:bg-primary/70 border-solid border border-zinc-900 hover:border hover:border-white
             transition-colors ease-out"
           >
             Sign out
